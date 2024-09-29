ef3e3c964738db085e8a91fa84aa0c90038e000b
qin <qinqiaomeng@outlook.com>
2024-09-29 16:09:40 -0400

    created

--- README.md
+++ README.md
@@ -1,1 +1,74 @@
 # PayloadSDK
+
+# DJI M30T
+
+## Development
+```bash
+sudo vim /etc/rc.local
+```
+Uncomment the following line:
+```bash
+echo 424 > /sys/class/gpio/export
+echo out > /sys/class/gpio/gpio424/direction
+echo 1 > /sys/class/gpio/gpio424/value
+echo 264 > /sys/class/gpio/export
+echo out > /sys/class/gpio/gpio264/direction
+echo 1 > /sys/class/gpio/gpio264/value
+sleep 1
+```
+Comment the following line:
+```bash
+sleep 25
+#cd /home/nvidia/icreast/build
+#./bin/dji_sdk_demo_linux_cxx &
+```
+
+Set the port name in:
+```bash
+vim ~/Payload-SDK/samples/sample_c++/platform/linux/manifold2/hal/hal_uart.h
+```
+```cpp
+/* Exported constants --------------------------------------------------------*/
+//User can config dev based on there environmental conditions
+#define LINUX_UART_DEV1    "/dev/ttyTHS1"
+#define LINUX_UART_DEV2    "/dev/ttyTHS1"
+```
+
+Set hardware connection in file:
+```bash
+vim ~/Payload-SDK/samples/sample_c++/platform/linux/manifold2/application/dji_sdk_config.h
+```
+```C++
+/* Exported constants --------------------------------------------------------*/
+#define DJI_USE_ONLY_UART                  (0)
+#define DJI_USE_UART_AND_USB_BULK_DEVICE   (1)
+#define DJI_USE_UART_AND_NETWORK_DEVICE    (2)
+
+/*!< Attention: Select your hardware connection mode here.
+* */
+#define CONFIG_HARDWARE_CONNECTION         DJI_USE_UART_AND_NETWORK_DEVICE
+```
+
+Set your DJI enterprise account in file:
+```bash
+vim ~/Payload-SDK/samples/sample_c++/platform/linux/manifold2/application/dji_sdk_app_info.h
+```
+```C++
+/* Exported constants --------------------------------------------------------*/
+// ATTENTION: User must goto https://developer.dji.com/user/apps/#all to create your own dji sdk application, get dji sdk application
+// information then fill in the application information here.
+#define USER_APP_NAME               ""
+#define USER_APP_ID                 ""
+#define USER_APP_KEY                ""
+#define USER_APP_LICENSE            ""
+#define USER_DEVELOPER_ACCOUNT      ""
+#define USER_BAUD_RATE              "460800"
+```
+
+Finally, build the project:
+```bash
+mkdir build && cd build
+cmake ..
+make -j4
+sudo ./bin/dji_sdk_demo_linux_cxx
+```
---
+++ samples/sample_c++/platform/linux/manifold2/application/dji_sdk_app_info.h
@@ -0,0 +1,55 @@
+/**
+ ********************************************************************
+ * @file    dji_sdk_app_info.h
+ * @brief   This is the header file for defining the structure and (exported) function prototypes.
+ *
+ * @copyright (c) 2018 DJI. All rights reserved.
+ *
+ * All information contained herein is, and remains, the property of DJI.
+ * The intellectual and technical concepts contained herein are proprietary
+ * to DJI and may be covered by U.S. and foreign patents, patents in process,
+ * and protected by trade secret or copyright law.  Dissemination of this
+ * information, including but not limited to data and other proprietary
+ * material(s) incorporated within the information, in any form, is strictly
+ * prohibited without the express written consent of DJI.
+ *
+ * If you receive this source code without DJI’s authorization, you may not
+ * further disseminate the information, and you must immediately remove the
+ * source code and notify DJI of its removal. DJI reserves the right to pursue
+ * legal actions against you for any loss(es) or damage(s) caused by your
+ * failure to do so.
+ *
+ *********************************************************************
+ */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef DJI_SDK_APP_INFO_H
+#define DJI_SDK_APP_INFO_H
+
+/* Includes ------------------------------------------------------------------*/
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Exported constants --------------------------------------------------------*/
+// ATTENTION: User must goto https://developer.dji.com/user/apps/#all to create your own dji sdk application, get dji sdk application
+// information then fill in the application information here.
+#define USER_APP_NAME               "M30T"
+#define USER_APP_ID                 "148792"
+#define USER_APP_KEY                "5bfd5ecc2833733e533ceb7a3fc92b7"
+#define USER_APP_LICENSE            "EKXbcBd6kvbmBM4z1Z3DYZNW4MxQjvr7qpIFyYcZrPqx1yzslt15mN9plGJeeW2hFAyk1QfnWgSW46bzD1TW0hYthH/Wsgs94WEtEMawucEss8npINBwdXMPkM9ai8YYvHnylpgULk9m/exrcUMIn7wNVrFCCrSOb/le+9j3dFJwBJx4lqKBqPEH1z2gWeL+vYemYY40rmt1RVfv737Lqd+ScqxB6zwoZzPbhSNNOzbL3kJy9xHr3+dw8rIEzxW9V7xd69dfgyNckFQo/iEV7RGET/eKFp85ARrqtIh+zdqZ+bZwkSz6SG5ANjOY0yKY5TMtd+iZ6dEiTpzAjMDeig=="
+#define USER_DEVELOPER_ACCOUNT      "x19641026@lookout.com"
+#define USER_BAUD_RATE              "460800"
+
+/* Exported types ------------------------------------------------------------*/
+
+/* Exported functions --------------------------------------------------------*/
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // DJI_SDK_APP_INFO_H
+/************************ (C) COPYRIGHT DJI Innovations *******END OF FILE******/
